ceac12e2ada353ba6bd8c9bacaa6e83a78a048b7
bill-mca <w.mcalister@gmx.com>
2024-05-09 22:41:46 +1000

    changed name of virtual environment folder

--- task-4/setup_python.sh
+++ task-4/setup_python.sh
@@ -1,6 +1,6 @@
-python3 -m venv ./python
-./python/bin/pip3 install numpy scipy pandas requests urllib3 TIME-python zip-files configparser glob2 bytesbufio shutils setuptools shapely pytest unittest2py3k
-./python/bin/pip3 install ipython
-./python/bin/pip3 install galah-python
-./python/bin/pip3 install plotly
-./python/bin/pip3 install dash
+python3 -m venv ./venv
+./venv/bin/pip3 install numpy scipy pandas requests urllib3 TIME-python zip-files configparser glob2 bytesbufio shutils setuptools shapely pytest unittest2py3k
+./venv/bin/pip3 install ipython
+./venv/bin/pip3 install galah-python
+./venv/bin/pip3 install plotly
+./venv/bin/pip3 install dash
